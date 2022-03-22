5ae09dc90c372e02a58d5911e8466c77e4e32a77
Steve Alomia <stevealomia@yahoo.com>
2022-03-22 11:48:51 -0400

    commit tree 5e43a8edaacca8c516b5e8d73b538c4436e07e4c

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(data => {
+    renderBooks(data);
+  }
+  )}
+
   
-}
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +20,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
